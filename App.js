2074c7388e7b56576bfd6a50b2b02d3488819dae
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2023-01-15 17:53:01 +0100

    ListingsScreen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
@@ -10,7 +11,18 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
+  );
 }
---
+++ app/components/AppTextInput.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextInput } from "react-native";
+
+const AppTextInput = () => {
+  return <TextInput></TextInput>;
+};
+
+export default AppTextInput;
